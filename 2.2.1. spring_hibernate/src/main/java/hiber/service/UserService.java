2220f878d1b3072fb77960f5e71f21d1333e8dc7
Kamran <kamran.07@icloud>
2023-08-21 12:47:02 +0300

    first commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+    List<User> getUserByCar(String model, int series);
+
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring_2.2.1_spring_hibernate
